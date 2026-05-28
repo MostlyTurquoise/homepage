2145233bedb0a131c6598f12bc36554c395a4bb1
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2026-05-28 20:14:02 +0100

    Add the beginnings of the index page, + strip out ab unch of stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,47 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	fonts: [
-		{
-			provider: fontProviders.local(),
-			name: 'Atkinson',
-			cssVariable: '--font-atkinson',
-			fallbacks: ['sans-serif'],
-			options: {
-				variants: [
-					{
-						src: ['./src/assets/fonts/atkinson-regular.woff'],
-						weight: 400,
-						style: 'normal',
-						display: 'swap',
-					},
-					{
-						src: ['./src/assets/fonts/atkinson-bold.woff'],
-						weight: 700,
-						style: 'normal',
-						display: 'swap',
-					},
-				],
-			},
-		},
-	],
+    site: "https://example.com",
+    integrations: [mdx(), sitemap()],
+    fonts: [
+        {
+            provider: fontProviders.local(),
+            name: "Day Poster Black",
+            cssVariable: "--font-day-poster-black",
+            fallbacks: ["mono"],
+            options: {
+                variants: [
+                    {
+                        src: [
+                            "./src/assets/fonts/Day Poster Black/DAYPBL__.ttf",
+                        ],
+                        weight: 400,
+                        style: "normal",
+                        display: "swap",
+                    },
+                ],
+            },
+        },
+        {
+            provider: fontProviders.local(),
+            name: "DECTERM",
+            cssVariable: "--font-decterm",
+            fallbacks: ["mono"],
+            options: {
+                variants: [
+                    {
+                        src: ["./src/assets/fonts/DECTERM/decterm.ttf"],
+                        weight: 400,
+                        style: "normal",
+                        display: "swap",
+                    },
+                ],
+            },
+        },
+    ],
 });
