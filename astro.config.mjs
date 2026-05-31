23b2fbd8e0960b06e7ee2f63e02b20efd83a35cf
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2026-05-31 16:40:38 +0100

    Try removing site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mostly.turquoise.fyi",
+    // site: "https://mostly.turquoise.fyi",
     integrations: [mdx(), sitemap()],
     fonts: [
         {
