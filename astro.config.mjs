0f4a5a0271768d7e897e36a25628d9f665cde7ae
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2026-05-31 16:19:11 +0100

    Add dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig, fontProviders } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
