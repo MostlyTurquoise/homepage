052f610aa25d02961bc7501314f13bd8831545c7
MostlyTurquoise <107848704+MostlyTurquoise@users.noreply.github.com>
2026-05-31 16:34:41 +0100

    Correct site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://example.com",
+    site: "https://mostly.turquoise.fyi",
     integrations: [mdx(), sitemap()],
     fonts: [
         {
